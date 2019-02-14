5343858370797a0bf9d9ed2806957dca488e99da
ejazsidhu <ejazsidhu@gmail.com>
2019-02-14 11:46:25 +0500

    filter added and envoirment set

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
-let href= window.location.href;
-let hrefArray=new Array();
-hrefArray=href.split(':');
+// let href= window.location.href;
+// let hrefArray=new Array();
+// hrefArray=href.split(':');
 
-let f=hrefArray[2];
-let hrefArray2=new Array();
-hrefArray2=f.split('/')
+// let f=hrefArray[2];
+// let hrefArray2=new Array();
+// hrefArray2=f.split('/')
 
-console.log("href",hrefArray2[0]);
-let port='';
-port=hrefArray2[0]
+// console.log("href",hrefArray2[0]);
+// let port='';
+// port=hrefArray2[0]
 
 export const environment = {
   production: true,
